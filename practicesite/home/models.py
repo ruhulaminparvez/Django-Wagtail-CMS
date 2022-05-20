b0f0eb2a3d87bcdf8960da50e897eaca08a668b9
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-20 19:56:10 +0600

    Updated

--- practicesite/home/models.py
+++ practicesite/home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
-    pass
+    """Home Page Model"""
+
+    templates = "home/home_page.html"
+
+    banner_title = models.CharField(max_length=255, blank=False, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ]

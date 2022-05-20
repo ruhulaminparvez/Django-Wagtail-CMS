48ee1ade4d04b7848bfb58a9ae2bc2bfad2482af
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-20 19:19:35 +0600

    Initial Setup

---
+++ practicesite/practicesite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure--dr8ytds)rltlbr87y*d8a)ttg@jj=0wcuhr=_$rii_&=gtb+e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
